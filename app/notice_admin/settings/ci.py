e3e3f08ab08e3e35e3f5278e7a8f0629c2ce1cfe
Ruslan Sibgatulin <doolf@yandex.ru>
2022-12-14 11:48:34 +0300

    Default env

--- app/notice_admin/settings/ci.py
+++ app/notice_admin/settings/ci.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "ci-secret")
 
 DEBUG = False
 
